68173483775902b7ccd7148d43bf6d3ba2ce4b7c
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-12-20 17:01:12 -0500

    Updated libraries and started Application class

---
+++ src/JavaFX/BlackJackGame.java
@@ -0,0 +1,18 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BlackJackGame extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
